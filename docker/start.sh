81a942849976cadbc20e9b4a035fae7d4f7d5ca3
inniy2 <inniy2@gmail.com>
2020-05-05 15:51:13 +0300

    support multi grpc-server

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm --name  envoy -d -p 9901:9901 -p 10000:10000 envoy:v1
+docker run --rm --name  envoy -d -p 9901:9901 -p 10000:10000 -p 10001:10001 envoy:v1
 
 docker container ls -a
